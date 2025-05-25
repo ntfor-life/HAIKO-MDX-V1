a83a77f84f70dc7b35044b4bd637c542c3aee9ff
PROFESSEURMDX <professeurmdx@gmail.com>
2025-05-25 08:03:35 -0400

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -107,10 +107,10 @@ const menu = async (m, Matrix) => {
           return Buffer.from(response.data, 'binary');
         } catch (error) {
           console.error('Error fetching menu image from URL, falling back to local image:', error);
-          return fs.readFileSync('https://files.catbox.moe/82nyf5.jpg');
+          return fs.readFileSync('https://files.catbox.moe/bqwb91.jpg');
         }
       } else {
-        return fs.readFileSync('https://files.catbox.moe/82nyf5.jpg');
+        return fs.readFileSync('https://files.catbox.moe/bqwb91.jpg');
       }
     };
 
